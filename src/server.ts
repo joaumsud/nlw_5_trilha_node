596fec3c5190c3f82291dfd667e56c34e520aecc
JoauMSuD <joaumsud@hotmail.com>
2021-04-19 09:31:02 -0300

    configurando o insomnia e metodos get e post

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,15 @@ const app = express();
 
 app.get("/", (request, response) => {
     return response.json({
-        message: "Olá NLW 05"
+        message: "Olá NLW 05!"
     });
-})
+});
+app.post("/", (request, response) => {
+    return response.json({
+        message: "Usuário salvo com sucesso!"
+    })
+
+});
 
 app.listen(3333, () => console.log("Server is running on  port 3333"));
 
