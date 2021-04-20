4aa3b3e26c1a14034ca9048582ecf477d5a716e9
JoauMSuD <joaumsud@hotmail.com>
2021-04-20 10:58:07 -0300

    configurando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,17 @@
 import express from "express";
 import "./database";
+import { routes } from "./routes";
+
 const app = express();
 
-/** */
+app.use(express.json());
 
+app.use(routes);
+
+app.listen(3333, () => console.log("Server is running on  port 3333"));
+
+/**
+Aprendendo sobre rotas, não necessário mais após a utilização do arquivo routes.ts
 app.get("/", (request, response) => {
     return response.json({
         message: "Olá NLW 05!"
@@ -14,7 +22,7 @@ app.post("/", (request, response) => {
         message: "Usuário salvo com sucesso!"
     })
 
-});
+});*/
 
-app.listen(3333, () => console.log("Server is running on  port 3333"));
+
 
