6b8d068f0cbd50af06564455b5b33e99714c91cc
JoauMSuD <joaumsud@hotmail.com>
2021-04-22 10:24:15 -0300

    Aprendendo websocket

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,5 +1,4 @@
 import { Entity, Column, CreateDateColumn, PrimaryColumn } from "typeorm";
-
 import { v4 as uuid } from "uuid";
 
 @Entity("users")
@@ -21,7 +20,5 @@ class User {
     }
 
 }
-
-
 
 export { User }
--- src/http.ts
+++ src/http.ts
@@ -27,23 +27,3 @@ io.on("connection", (socket: Socket) => {
 app.use(express.json());
 
 app.use(routes);
-
-http.listen(3333, () => console.log("Server is running on  port 3333"));
-//app.listen(3333, () => console.log("Server is running on  port 3333"));
-
-/**
-Aprendendo sobre rotas, não necessário mais após a utilização do arquivo routes.ts
-app.get("/", (request, response) => {
-    return response.json({
-        message: "Olá NLW 05!"
-    });
-});
-app.post("/", (request, response) => {
-    return response.json({
-        message: "Usuário salvo com sucesso!"
-    })
-
-});*/
-
-
-
